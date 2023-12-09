aa0c3578f4319c8a5bd1aa857954eae357f8f362
Krunal-Karena <krunalkarena26@gmail.com>
2023-12-09 16:27:59 +0530

    5. Token transfer

--- test/Token.js
+++ test/Token.js
@@ -6,13 +6,14 @@ const tokens = (n)=>{
 }
 
 describe("Token",()=>{
-    let token,accounts,deployer;
+    let token,accounts,deployer,receiver;
     beforeEach(async ()=>{
         //get token from blockchain
         const Token = await ethers.getContractFactory("Token");
         token = await Token.deploy("My Token","DAPP",1000000);
         accounts = await ethers.getSigners();
         deployer = accounts[0];
+        receiver = accounts[1];
     })
 
     describe("Deployment",()=>{
@@ -41,4 +42,42 @@ describe("Token",()=>{
         })
     })
 
+    describe("Sending Tokens",()=>{
+        let amount,transaction,result;
+
+        describe('Success',async ()=>{
+            beforeEach(async ()=>{
+                amount=tokens(100);
+                transaction =await token.connect(deployer).transfer(receiver.address,amount);
+                result =await transaction.wait();
+            })
+    
+            it('transfer token balances',async ()=>{
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900));
+                expect(await token.balanceOf(receiver.address)).to.equal(amount);
+            })
+    
+            it('emits a transfer event',()=>{
+                const event = result.events[0];
+                expect(event.event).to.equal('Transfer');
+    
+                const arg=event.args;
+                expect(arg.from).to.equal(deployer.address);
+                expect(arg.to).to.equal(receiver.address);
+                expect(arg.value).to.equal(amount);
+            })
+        })
+        
+        describe('Failure',async() =>{
+            it('rejects insufficient balance',async()=>{
+                const invalidAmount = tokens(100000000);
+                expect(token.connect(deployer).transfer(receiver.address,invalidAmount)).to.be.reverted;
+            })
+
+            it('rejects invalid recipent',async()=>{
+                const amount = tokens(100);
+                expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000',amount)).to.be.reverted;
+            })
+        })
+    })
 })
